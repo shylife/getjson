c6461c7081ea23d58c975b9cd9b0aaf8d8dfd9df
user1 <haklife47815@gmail.com>
2022-03-11 04:37:58 +0900

    첫 저장.

---
+++ app/src/main/java/com/example/getjson/MainActivity.java
@@ -0,0 +1,148 @@
+package com.example.getjson;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "phptest_MainActivity";
+
+    private static final String TAG_JSON="webnautes";
+    private static final String TAG_ID = "name";
+    private static final String TAG_NAME = "job";
+    private static final String TAG_ADDRESS ="many";
+
+    private static TextView mTextViewResult;
+    ArrayList<HashMap<String, String>> mArrayList;
+    ListView mlistView;
+    String mJsonString;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.test);
+        mTextViewResult = (TextView)findViewById(R.id.test1);
+        MyAsynctask task = new MyAsynctask();
+        task.execute("http://192.168.0.201:5000/Android/getjson.php");
+        // 안드로이드 스튜디오 Emulator 로는 localhost:포트번호 접근이 불가능하다.
+        // + 안드로이드는 기본적으로 HTTP conn 을 금지한다 - manifest 수정이 필요.
+    }
+
+    public static class MyAsynctask extends AsyncTask<String, Void, String> {
+        public MyAsynctask(){
+            super();
+        }
+        @Override
+        protected void onPreExecute(){
+            super.onPreExecute();
+        }
+        @Override
+        protected String doInBackground(String ... strings){
+            // 서브스레드로 처리되는 곳.
+            String serverURL = (String) strings[0];
+/*
+            String key = (String) strings[1];
+            String value = (String) strings[2];
+
+            String key2 = (String) strings[3];
+            String value2 = (String) strings[4];
+
+            String key3 = (String) strings[5];
+            String value3 = (String) strings[6];
+ */
+            String postParameters = "";
+            try {
+                // 2. HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+                URL url = new URL(serverURL); // 주소가 저장된 변수를 이곳에 입력합니다.
+
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+
+                httpURLConnection.setReadTimeout(5000); //5초안에 응답이 오지 않으면 예외가 발생합니다.
+
+                httpURLConnection.setConnectTimeout(5000); //5초안에 연결이 안되면 예외가 발생합니다.
+
+                httpURLConnection.setRequestMethod("POST"); //요청 방식을 POST로 합니다.
+                httpURLConnection.connect();
+
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                outputStream.write(postParameters.getBytes("UTF-8")); //전송할 데이터가 저장된 변수를 이곳에 입력합니다.
+
+                outputStream.flush();
+                outputStream.close();
+
+                // 응답을 읽습니다.
+
+                int responseStatusCode = httpURLConnection.getResponseCode();
+                Log.d(TAG, "POST response code - " + responseStatusCode);
+
+                InputStream inputStream;
+                if (responseStatusCode == HttpURLConnection.HTTP_OK) {
+
+                    // 정상적인 응답 데이터
+                    inputStream = httpURLConnection.getInputStream();
+                } else {
+
+                    // 에러 발생
+
+                    inputStream = httpURLConnection.getErrorStream();
+                }
+
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    sb.append(line);
+                }
+                bufferedReader.close();
+
+                return sb.toString();
+
+            } catch (Exception e) {
+
+                Log.d(TAG, "InsertData: Error ", e);
+
+                return new String("Error: " + e.getMessage());
+            }
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            //progressDialog.dismiss();
+            mTextViewResult.setText(result);
+            Log.d(TAG, "POST response  - " + result);
+        }
+
+    }
+}
